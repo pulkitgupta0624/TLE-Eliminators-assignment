811b11e6977e1b80ad405032b3f87f3e17bdcfd3
pulkitgupta0624 <pulkitgupta2403@gmail.com>
2026-01-02 21:11:56 +0530

    new commits

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -177,8 +177,8 @@ export const login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: config.nodeEnv === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000
     });
 
